f53bd3f71a65960a2ccf34f44e1c3603bb0374e7
simranbirla <ssbirla01@gmail.com>
2024-08-01 18:04:46 +0530

    chore: header

---
+++ client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import "./navbar.scss";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="left-side">
+        <a href="/" className="logo">
+          <div>Logo</div>
+          <span>Real Estate App</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
+        <a href="/agents">Agents</a>
+      </div>
+
+      <div className="right-side">
+        <a href="/sign-in" className="sign-in">
+          Sign In
+        </a>
+        <a href="/sign-up" className="sign-up">
+          Sign Up
+        </a>
+      </div>
+
+      <div className="menu-button">
+        <GiHamburgerMenu />
+      </div>
+
+      <div className="horizontal-nav">
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
+        <a href="/agents">Agents</a>
+        <a href="/sign-in">Sign In</a>
+        <a href="/sign-up">Sign Up</a>
+      </div>
+    </nav>
+  );
+}
