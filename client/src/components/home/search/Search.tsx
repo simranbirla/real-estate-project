13196fde5acfd0f7b784bac6ad0546f0e2bb747f
simranbirla <ssbirla01@gmail.com>
2024-08-08 12:03:21 +0530

    chore: css changes

--- client/src/components/home/search/Search.tsx
+++ client/src/components/home/search/Search.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import { CiSearch } from "react-icons/ci";
+import "./search.scss";
+import { useState } from "react";
 
 export default function Search() {
-  return <div>Search</div>;
+  const [details, setDetails] = useState({
+    type: "buy",
+    location: "",
+    min: "",
+    max: "",
+  });
+
+  const handleSelectType = (value: string) => {
+    setDetails((prev) => ({ ...prev, type: value }));
+  };
+
+  return (
+    <div className="search">
+      <div className="search-types">
+        <div
+          className={`${details.type === "buy" ? "active" : ""}`}
+          onClick={() => handleSelectType("buy")}
+        >
+          Buy
+        </div>
+        <div
+          className={`${details.type === "rent" ? "active" : ""}`}
+          onClick={() => handleSelectType("rent")}
+        >
+          Rent
+        </div>
+      </div>
+      <form>
+        <input type="text" placeholder="City Location" />
+        <input type="number" placeholder="Min Price" />
+        <input type="number" placeholder="Max Price" />
+        <button type="submit">
+          <CiSearch />
+        </button>
+      </form>
+    </div>
+  );
 }
