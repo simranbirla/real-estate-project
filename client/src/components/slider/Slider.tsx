23b34d19188645397af517ee00c2de6650debbb9
simranbirla <ssbirla01@gmail.com>
2024-08-10 12:56:34 +0530

    chore: slider photos placement

--- client/src/components/slider/Slider.tsx
+++ client/src/components/slider/Slider.tsx
@@ -1,5 +1,16 @@
 import "./slider.scss";
 
 export default function Slider({ images }: { images: string[] }) {
-  return <div>Slider</div>;
+  return (
+    <div className="slider">
+      <div className="big">
+        <img src={images[0]} alt="Property Main Image " />
+      </div>
+      <div className="small">
+        {images.slice(1).map((img, key) => (
+          <img src={img} alt={`Property Images ${key + 1} `} />
+        ))}
+      </div>
+    </div>
+  );
 }
