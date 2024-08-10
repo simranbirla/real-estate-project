0446f055ed132ab9ecd33904b54146e6e8b7acf6
simranbirla <ssbirla01@gmail.com>
2024-08-10 12:50:10 +0530

    chore: slider scss

--- client/src/components/prop/SingleProp.tsx
+++ client/src/components/prop/SingleProp.tsx
@@ -1,3 +1,37 @@
+import Slider from "../slider/Slider";
+import "./prop.scss";
+import { singlePostData, userData } from "../../dummyData";
+import { CiLocationOn } from "react-icons/ci";
+
 export default function SingleProp() {
-  return <div>SingleProp</div>;
+  return (
+    <div className="prop">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <CiLocationOn />
+                  {singlePostData.address}
+                </div>
+                <div className="price">${singlePostData.price}</div>
+              </div>
+
+              <div className="user">
+                <img src={userData.img} alt={userData.name} />
+                <p>{userData.name}</p>
+              </div>
+            </div>
+            <div className="bottom">{singlePostData.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 }
---
+++ client/src/components/slider/Slider.tsx
@@ -0,0 +1,5 @@
+import "./slider.scss";
+
+export default function Slider({ images }: { images: string[] }) {
+  return <div>Slider</div>;
+}
