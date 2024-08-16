793e3e24129a9725c29970d083dd078edfbc3692
simranbirla <ssbirla01@gmail.com>
2024-08-16 17:59:23 +0530

    chore: css changes

--- client/src/components/home/HomePage.tsx
+++ client/src/components/home/HomePage.tsx
@@ -5,28 +5,30 @@ export default function HomePage() {
   return (
     <div className="home">
       <div className="left-side">
-        <div className="title">
-          Find your dream place that you can rent or buy
-        </div>
-        <div className="sub-title">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-          officiis quam commodi labore esse, illum dolores explicabo nam quae
-          cumque nihil ratione dolorum, quos illo numquam? Fugit culpa
-          consectetur laboriosam.
-        </div>
-        <Search />
-        <div className="stats">
-          <div className="stats-details">
-            10+
-            <span>Years of experience</span>
+        <div className="wrapper">
+          <div className="title">
+            Find your dream place that you can rent or buy
           </div>
-          <div className="stats-details">
-            300
-            <span>Awards Gained</span>
+          <div className="sub-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            officiis quam commodi labore esse, illum dolores explicabo nam quae
+            cumque nihil ratione dolorum, quos illo numquam? Fugit culpa
+            consectetur laboriosam.
           </div>
-          <div className="stats-details">
-            10k+
-            <span>Happy Customers</span>
+          <Search />
+          <div className="stats">
+            <div className="stats-details">
+              10+
+              <span>Years of experience</span>
+            </div>
+            <div className="stats-details">
+              300
+              <span>Awards Gained</span>
+            </div>
+            <div className="stats-details">
+              10k+
+              <span>Happy Customers</span>
+            </div>
           </div>
         </div>
       </div>
