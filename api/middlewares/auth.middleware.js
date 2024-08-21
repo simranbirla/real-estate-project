90fee9bfcb196cb3f3e8148b1c0a4d1a10d281a6
simranbirla <ssbirla01@gmail.com>
2024-08-21 17:08:20 +0530

    fix: token

--- api/middlewares/auth.middleware.js
+++ api/middlewares/auth.middleware.js
@@ -4,10 +4,16 @@ export const verifyToken = async (req, res, next) => {
     try {
         const token = req.cookies.access_token;
 
+        if (!token) {
+            return res.status(403).json({
+                error: "Not Authenticated"
+            })
+        }
+
         jwt.verify(token, process.env.JWT_SECRET, function (err, payload) {
             if (err) {
                 return res.status(403).json({
-                    error: "Not Authenticated"
+                    error: "Token not valid"
                 })
             }
             next()
