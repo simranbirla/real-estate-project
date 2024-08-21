e7ee1ed8c0517c097da74cf7340683a48337d6f8
simranbirla <ssbirla01@gmail.com>
2024-08-21 12:47:55 +0530

    feat: login

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,21 +1,55 @@
+import { useState } from "react";
+import apiRequest from "../../lib/apiRequest.js";
 import "./login.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    try {
+      setIsLoading(true)
+      e.preventDefault()
+
+      const formData = new FormData(e.target)
+
+      const username = formData.get("username")
+      const password = formData.get("password")
+
+      const result = await apiRequest.post("/auth/login", {
+        username, password
+      })
+
+      navigate("/")
+    } catch (e) {
+      console.log(e)
+      setError(e.response?.data.error ?? "Error")
+    } finally {
+      setIsLoading(false)
+    }
+
+  }
+
   return (
     <div className="login">
-      <div className="formContainer">
-        <form>
+      {isLoading ? <div className="loading">Loading</div> : <><div className="formContainer">
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="password" type="password" placeholder="Password" />
           <button>Login</button>
           <Link to="/register">{"Don't"} you have an account?</Link>
+          {error && <span>{error}</span>}
         </form>
       </div>
-      <div className="imgContainer">
-        <img src="/bg.png" alt="" />
-      </div>
+        <div className="imgContainer">
+          <img src="/bg.png" alt="" />
+        </div>
+      </>
+      }
     </div>
   );
 }
--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,22 +1,60 @@
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import apiRequest from '../../lib/apiRequest.js';
 
 function Register() {
+
+  const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    try {
+      setIsLoading(true)
+      e.preventDefault()
+
+      const formData = new FormData(e.target)
+
+      const username = formData.get("username")
+      const email = formData.get("email")
+      const password = formData.get("password")
+
+      console.log(username, email, password)
+
+      const result = await apiRequest.post("/auth/register", {
+        username, email, password
+      })
+
+
+      navigate("/login")
+    } catch (e) {
+      setError(e.response.data.error ?? "Something went wrong")
+    } finally {
+      setIsLoading(false)
+    }
+
+  }
+
   return (
     <div className="register">
-      <div className="formContainer">
-        <form>
+      {isLoading ? <div className="loading">Loading</div> : <><div className="formContainer">
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
           <button >Register</button>
           <Link to="/login">Do you have an account?</Link>
+          {error && <span>{error}</span>}
         </form>
       </div>
-      <div className="imgContainer">
-        <img src="/bg.png" alt="" />
-      </div>
+        <div className="imgContainer">
+          <img src="/bg.png" alt="" />
+        </div>
+      </>
+      }
     </div>
   );
 }
